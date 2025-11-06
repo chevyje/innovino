f9a7dad71982ec55fdc6b5d58b1c6e3b0bd28ab3
chevyje <jurre.blankers@gmail.com>
2025-11-06 12:44:25 +0100

    Added cors to the backend that we could use it in the frontend

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
-from database import select_db
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.users import router as users_router
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users_router, prefix="/api")
