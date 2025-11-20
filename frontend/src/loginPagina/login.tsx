22f522edf769a66d706bd850cc74b557b8938fcd
martyparty06 <144609720+martyparty06@users.noreply.github.com>
2025-11-20 10:33:30 +0100

    Inlogpagina v0.1
    halverwege af

--- frontend/src/loginPagina/login.tsx
+++ frontend/src/loginPagina/login.tsx
@@ -1,5 +1,5 @@
 import Style from "./login.module.css"
-import bg from "../assets/inlog-background.jpg"
+import Logo from "../assets/Cuimed-logo.jpg"
 
 export default function loginPage() {
     function loginSubmit (formData: any){
@@ -9,15 +9,16 @@ export default function loginPage() {
     }
     return (
         <>
-            <div className={Style.background} style={{backgroundImage: `url(${bg})` }}>
+            <div className={Style.background} style={{backgroundImage: "none" }}>
                 <div className={Style.wrapper}>
-                    <h1 className={Style.title}>Inloggen</h1>
+                    <img src={Logo} className={Style.logo} />
                     <div className={Style.loginContainer}>
                         <form action={loginSubmit} className={Style.loginForm}>
                             <p className={Style.tag}>Gebruikersnaam</p>
                             <input type={"text"} name={"username"} className={Style.inputField} />
                             <p className={Style.tag}>Wachtwoord</p>
                             <input type={"password"} name={"password"} className={Style.inputField}/>
+                            
                             <button className={Style.button}>Log in</button>
                         </form>
                     </div>
