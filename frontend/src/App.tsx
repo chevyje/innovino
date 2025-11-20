fc9157aebc1b8a989814b6670ac64392bad761ed
chevyje <jurre.blankers@gmail.com>
2025-11-20 18:54:28 +0100

    Login pagina heeft nu een eigen route en bestaat weer met import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TestPage from "./testPage/test.tsx";
+import LoginPage from "./loginPagina/login.tsx"
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<LoginPage />} />
                 <Route path="/test" element={<TestPage />} />
             </Routes>
-            <LoginPage />
         </BrowserRouter>
     );
 }
