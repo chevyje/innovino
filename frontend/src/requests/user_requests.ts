bc3e434562b06d49454672112e376f818bb76d71
chevyje <jurre.blankers@gmail.com>
2025-11-20 18:55:43 +0100

    Stores the session id when a successful login happens into a cookie, we could use this cookie to get it and use it in the headers for the api key

--- frontend/src/requests/user_requests.ts
+++ frontend/src/requests/user_requests.ts
@@ -1,5 +1,6 @@
 import type {LoginRequest} from "../models/user_model.ts";
 import {getHeaders, baseUrl} from "../utils/requests.ts";
+import {setCookie} from "../utils/cookies.ts";
 
 export async function authenticate(data: LoginRequest) {
     const headers: Headers = getHeaders()
@@ -13,5 +14,9 @@ export async function authenticate(data: LoginRequest) {
     // Return something that the frontend can use
     const res = await fetch(request);
     const response_data = await res.json();
-    return alert(JSON.stringify(response_data));
+
+    setCookie('session_id', response_data.session.session_id, new Date(response_data.session.expires_at));
+    console.log(response_data.session);
+    console.log(new Date(response_data.session.expires_at));
+    return response_data;
 }
--- frontend/src/utils/cookies.ts
+++ frontend/src/utils/cookies.ts
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie'
 
 export function setCookie(name: string, value: string, expiration_date: Date){
-    Cookies.set(name, value, expiration_date)
+    Cookies.set(name, value, { expires: expiration_date })
 }
